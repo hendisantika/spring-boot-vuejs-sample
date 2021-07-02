9465bae88cc90bca3fa85900c7c9a35952d6b71e
Hendi Santika <hendisantika@gmail.com>
2021-07-02 18:52:18 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootVuejsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVuejsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVuejsSampleApplication.class, args);
+    }
+
+}
